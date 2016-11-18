3889914d3bdee9f147f81f5d18f09d38b185ad5e
zmz0305 <zmz305@gmail.com>
2016-11-18 13:26:54 -0600

    app

--- app.js
+++ app.js
@@ -102,6 +102,7 @@ app.get("/initState", function (req, res) {
             callback(null, val);
         })
     }], function (err, results) {
+        console.log(results);
         var val = 0;
         for(var i = 0; i < results.length; i++){
             val |= results[i];
